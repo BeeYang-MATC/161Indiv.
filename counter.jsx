458726d159b6c0a72ca3be7fb6bf8ad666170fa7
Bee Yang <35712411+BeeYang-MATC@users.noreply.github.com>
2018-12-09 13:09:35 -0600

    Add files via upload

--- counter.jsx
+++ counter.jsx
@@ -2,25 +2,28 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
-  };
-
-  handleIncrement = product => {
-    console.log(product);
-    //used arrow fuction in place of the using the constructor
-    //this methold will print to the console "increment clicked"  when the user press the button
-    this.setState({ count: this.state.count + 1 }); // this will tell react what has changed and react will syinc with dom- everytime the button is pushed the count is counted and displayed
+    imageUrl: "https://picsum.photos/200"
   };
 
   render() {
+    console.log("Counter - Rendered");
+
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <img src={this.state.imageUrl} alt="" />
+        <span className={this.getBadgeClasses()}>{this.formatvalue()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          Quantity
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete Row
         </button>
       </div>
     );
@@ -28,15 +31,15 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
     // gave the badges a name called classess
   }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-    //method formatCount; if count = 0 than it should print the word zero
+  formatvalue() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
+    //method formatvalue; if value = 0 than it should print the word zero
   }
 }
 
